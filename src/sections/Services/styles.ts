e908532a5705dcb91a9bdbb28b12e8a3d05be944
Deivith <deivithfagundes@gmail.com>
2026-01-27 13:15:28 -0300

    fetch: Responsividade para seção de serviços

--- src/sections/Services/styles.ts
+++ src/sections/Services/styles.ts
@@ -34,13 +34,13 @@ export const CardList = styled.ul`
     grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: 32px;
 
-    // @media (max-width: ${breakpoints.tablet}) {
-    //     grid-template-columns: 1fr 1fr;
-    // }
+    @media (max-width: ${breakpoints.tablet}) {
+        grid-template-columns: 1fr 1fr;
+    }
 
-    // @media (max-width: ${breakpoints.mobile}) {
-    //     grid-template-columns: 1fr;
-    // }
+    @media (max-width: ${breakpoints.mobile}) {
+        grid-template-columns: 1fr;
+    }
 `
 
 export const CardItem = styled.li`
